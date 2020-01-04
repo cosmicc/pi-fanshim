5732cdc209696fb0a058103e3a3ee0c0142ed099
unknown <ianperry99@gmail.com>
2020-01-04 14:55:05 -0500

    update

--- gpsupdate.py
+++ gpsupdate.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import time
-from tzwhere import tzwhere
+
 import gps
 import maidenhead as mh
+from tzwhere import tzwhere
 
 gpsd = gps.gps(mode=gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 tz = tzwhere.tzwhere()
@@ -15,4 +16,6 @@ while True:
         lon = getattr(report, 'lon', 0.0)
         mhead = mh.toMaiden(lat, lon, precision=4)
         print(f'lat: {lat}, lon: {lon}, maiden: {mhead}, {tz.tzNameAt(lat, lon)}')
-        time.sleep(10)
+        fan_file = open("/dev/shm/gps", 'w')
+        fan_file.write(f'lat={lat}\nlon={lon}\nmaiden={mhead}\ntimezone={tz.tzNameAt(lat, lon)}')
+        time.sleep(30)
