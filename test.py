f29f684435cbb407a81ff05b20e02189e75c5046
unknown <ianperry99@gmail.com>
2020-01-04 11:26:25 -0500

    update

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 scale = 16
-num_of_bits = 19 
+num_of_bits = 19
 
 result = subprocess.run(['vcgencmd', 'get_throttled'], stdout=subprocess.PIPE)
 res = result.stdout.decode()
@@ -18,5 +18,26 @@ if b[0] == '1':
     throttle_hist = True
 else:
     throttle_hist = False
+if b[1] == '1':
+    cap_hist = True
+else:
+    cap_hist = False
+if b[2] == '1':
+    undervolt_hist = True
+else:
+    undervolt_hist = False
 
+if b[17] == '1':
+    throttle_now = True
+else:
+    throttle_now = False
+if b[18] == '1':
+    cap_now = True
+else:
+    cap_now = False
+if b[19] == '1':
+    undervolt_now = True
+else:
+    undervolt_now = False
 
+print(throttle_now, throttle_hist, cap_now, cap_hist, undervolt_now, undervolt_hist)
