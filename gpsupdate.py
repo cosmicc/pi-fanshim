9fc480bc188c8b51b0ac1297b05c0081e4ed7a41
root <ianperry99@gmail.com>
2020-01-04 14:51:28 -0500

    update

--- gpsupdate.py
+++ gpsupdate.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 import time
-
+from tzwhere import tzwhere
 import gps
 import maidenhead as mh
 
 gpsd = gps.gps(mode=gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+tz = tzwhere.tzwhere()
 
 while True:
     report = gpsd.next()
     if report['class'] == 'TPV':
-        lat = gps.getattr(report, 'lat', 0.0)
-        lon = gps.getattr(report, 'lon', 0.0)
-        mhead = mh.toMaiden(lat, lon, 4)
-        print(f'lat: {lat}, lon: {lon}, maiden: {mhead}')
-    time.sleep(10)
+        lat = getattr(report, 'lat', 0.0)
+        lon = getattr(report, 'lon', 0.0)
+        mhead = mh.toMaiden(lat, lon, precision=4)
+        print(f'lat: {lat}, lon: {lon}, maiden: {mhead}, {tz.tzNameAt(lat, lon)}')
+        time.sleep(10)
