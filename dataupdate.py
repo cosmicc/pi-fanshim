395b81b680e78ca2a5b5c35f014f806c3d476dd9
unknown <ianperry99@gmail.com>
2020-01-11 19:52:49 -0500

    updatep

--- dataupdate.py
+++ dataupdate.py
@@ -64,6 +64,44 @@ def remap(x, oMin, oMax, nMin, nMax):
     return int(result)
 
 
+def throttle_check():
+    scale = 16
+    num_of_bits = 19
+    result = subprocess.run(['vcgencmd', 'get_throttled'], stdout=subprocess.PIPE)
+    res = result.stdout.decode()
+    hexval = res.split('=')[1]
+    # hexval = "0x50005"
+    b = bin(int(hexval, scale))[2:].zfill(num_of_bits)
+    if b[0] == '1':
+        throttle_hist = True
+    else:
+        throttle_hist = False
+    if b[1] == '1':
+        cap_hist = True
+    else:
+        cap_hist = False
+    if b[2] == '1':
+        undervolt_hist = True
+    else:
+        undervolt_hist = False
+
+    if b[16] == '1':
+        throttle_now = True
+    else:
+        throttle_now = False
+    if b[17] == '1':
+        cap_now = True
+    else:
+        cap_now = False
+    if b[18] == '1':
+        undervolt_now = True
+    else:
+        undervolt_now = False
+
+    fan_file = open("/dev/shm/throttle", 'w')
+    fan_file.write(f'undervolt_now={undervolt_now}\nundervolt_hist={undervolt_hist}\nthrottle_now={throttle_now}\nthrottle_hist={throttle_hist}\ncpucap_now={cap_now}\ncpucap_hist={cap_hist}\n')
+
+
 def netcheck():
     child = subprocess.Popen(['iwconfig'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=False)
     streamdata = child.communicate()[0].decode('UTF-8').split('\n')
@@ -106,6 +144,7 @@ def netcheck():
 
 while True:
     netcheck()
+    throttle_check()
     try:
         lat = gpsd_shm.fix.latitude
     except:
